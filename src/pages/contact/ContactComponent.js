888949fae59377160dbc3a0624f2f17aa2cc8e41
vbcode19 <batravarun698@gmail.com>
2023-06-16 15:26:15 +0530

    form

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -16,8 +16,45 @@ const addressSection = contactPageData.addressSection;
 const phoneSection = contactPageData.phoneSection;
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    };
+  }
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    // Perform actions with the form data, such as sending it to a backend server
+    // You can access the form data using this.state.name, this.state.email, etc.
+
+    // Clear the form fields
+    this.setState({
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    });
+  };
+
   render() {
     const theme = this.props.theme;
+    const { name, email, subject, message } = this.state;
+
     return (
       <div className="contact-main">
         <Header theme={theme} />
@@ -78,9 +115,9 @@ class Contact extends Component {
               </div>
               <div className="blog-heading-img-div">
                 {/* <img
-											src={require(`../../assests/images/${blogSection["avatar_image_path"]}`)}
-											alt=""
-										/> */}
+                      src={require(`../../assests/images/${blogSection["avatar_image_path"]}`)}
+                      alt=""
+                    /> */}
                 <BlogsImg theme={theme} />
               </div>
             </div>
@@ -89,9 +126,9 @@ class Contact extends Component {
             <div className="address-heading-div">
               <div className="contact-heading-img-div">
                 {/* <img
-											src={require(`../../assests/images/${addressSection["avatar_image_path"]}`)}
-											alt=""
-										/> */}
+                      src={require(`../../assests/images/${addressSection["avatar_image_path"]}`)}
+                      alt=""
+                    /> */}
                 <AddressImg theme={theme} />
               </div>
               <div className="address-heading-text-div">
@@ -130,6 +167,57 @@ class Contact extends Component {
               </div>
             </div>
           </Fade>
+          <div className="contact-form-div">
+            <form onSubmit={this.handleSubmit}>
+              <h2>Contact Form</h2>
+              <div className="form-group">
+                <label htmlFor="name">Your Name</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={name}
+                  onChange={this.handleInputChange}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Your Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={this.handleInputChange}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="subject">Subject</label>
+                <input
+                  type="text"
+                  id="subject"
+                  name="subject"
+                  value={subject}
+                  onChange={this.handleInputChange}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="message">Message</label>
+                <textarea
+                  id="message"
+                  name="message"
+                  value={message}
+                  onChange={this.handleInputChange}
+                  required
+                ></textarea>
+              </div>
+              <div className="form-group">
+                <button type="submit">Submit</button>
+              </div>
+            </form>
+          </div>
         </div>
         <Footer theme={this.props.theme} onToggle={this.props.onToggle} />
         <TopButton theme={this.props.theme} />
